95e92bc4bc6f8bff77d23b54d47ec203d6c6fc80
stephen-cavallaro <Cavallaro.Stephen@gmail.com>
2021-01-15 17:13:52 -0500

    first initial commit

---
+++ test/specs/example.e2e.js
@@ -0,0 +1,15 @@
+const LoginPage = require('../../login.page');
+const SecurePage = require('../../secure.page');
+
+describe('My Login application', () => {
+    it('should login with valid credentials', () => {
+        LoginPage.open();
+
+        LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        expect(SecurePage.flashAlert).toBeExisting();
+        expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+});
+
+
